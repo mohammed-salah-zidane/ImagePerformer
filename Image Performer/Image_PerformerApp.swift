75a95a75d0dc2973f0ff5995df1590c733c94a27
Mohamed Salah <mohamed.zidane95@gmail.com>
2024-10-02 16:25:58 +0300

    create project

---
+++ Image Performer/Image_PerformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_PerformerApp.swift
+//  Image Performer
+//
+//  Created by Mohamed Salah on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Image_PerformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Image PerformerTests/Image_PerformerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Image_PerformerTests.swift
+//  Image PerformerTests
+//
+//  Created by Mohamed Salah on 02/10/2024.
+//
+
+import Testing
+
+struct Image_PerformerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
